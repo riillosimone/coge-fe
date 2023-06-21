12101b370962557a3dbb2657b018bf6c9147916c
Lorenzo <lorenzogra2022@libero.it>
2023-06-21 17:17:04 +0200

    commit per simone di nuovo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { WelcomeComponent } from './features/welcome/welcome.component';
 import { AziendaListComponent } from './features/azienda/azienda-list/azienda-list.component';
 import { AziendaShowComponent } from './features/azienda/azienda-show/azienda-show.component';
 import { AziendaInsertComponent } from './features/azienda/azienda-insert/azienda-insert.component';
-<<<<<<< Updated upstream
 import { AziendaEditComponent } from './features/azienda/azienda-edit/azienda-edit.component';
 import { RisorsaListComponent } from './features/risorsa/risorsa-list/risorsa-list.component';
 import { AziendaDeleteComponent } from './features/azienda/azienda-delete/azienda-delete.component';
 import { CommessaListComponent } from './features/commessa/commessa-list/commessa-list.component';
 import { CommessaShowComponent } from './features/commessa/commessa-show/commessa-show.component';
 import { CommessaInsertComponent } from './features/commessa/commessa-insert/commessa-insert.component';
+import { RisorsaShowComponent } from './features/risorsa/risorsa-show/risorsa-show.component';
+import { RisorsaEditComponent } from './features/risorsa/risorsa-edit/risorsa-edit.component';
+import { RisorsaInsertComponent } from './features/risorsa/risorsa-insert/risorsa-insert.component';
 
 const routes: Routes = [
   { path: 'azienda/list', component: AziendaListComponent },
@@ -18,26 +20,13 @@ const routes: Routes = [
   { path: 'azienda/edit/:id', component:AziendaEditComponent},
   { path: 'azienda/show/:id', component:AziendaShowComponent},
   { path: 'azienda/delete/:id', component:AziendaDeleteComponent},
-<<<<<<< Updated upstream
-=======
-import { RisorsaListComponent } from './features/risorsa/risorsa-list/risorsa-list.component';
-import { RisorsaShowComponent } from './features/risorsa/risorsa-show/risorsa-show.component';
-import { RisorsaInsertComponent } from './features/risorsa/risorsa-insert/risorsa-insert.component';
->>>>>>> Stashed changes
-
-=======
   { path: 'commessa/list', component: CommessaListComponent },
   { path: 'commessa/show/:id', component:CommessaShowComponent},
   { path: 'commessa/insert', component: CommessaInsertComponent},
->>>>>>> Stashed changes
-  { path: 'risorsa/list', component: RisorsaListComponent },
-<<<<<<< Updated upstream
-=======
-  { path: 'risorsa/show/:id', component:RisorsaShowComponent},
+  { path: 'risorsa/show/:id', component: RisorsaShowComponent },
+  { path: 'risorsa/edit/:id', component: RisorsaEditComponent },
   { path: 'risorsa/insert', component: RisorsaInsertComponent },
-  { path: 'azienda/insert', component: AziendaInsertComponent },
-  { path: 'azienda/show/:id', component:AziendaShowComponent},
->>>>>>> Stashed changes
+  { path: 'risorsa/list', component: RisorsaListComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,20 +13,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AziendaShowComponent } from './features/azienda/azienda-show/azienda-show.component';
 import { AziendaInsertComponent } from './features/azienda/azienda-insert/azienda-insert.component';
 import { RisorsaListComponent } from './features/risorsa/risorsa-list/risorsa-list.component';
-<<<<<<< Updated upstream
 import { AziendaEditComponent } from './features/azienda/azienda-edit/azienda-edit.component';
 import { AziendaDeleteComponent } from './features/azienda/azienda-delete/azienda-delete.component';
-<<<<<<< Updated upstream
-=======
 import { RisorsaShowComponent } from './features/risorsa/risorsa-show/risorsa-show.component';
 import { RisorsaInsertComponent } from './features/risorsa/risorsa-insert/risorsa-insert.component';
 import { RisorsaEditComponent } from './features/risorsa/risorsa-edit/risorsa-edit.component';
->>>>>>> Stashed changes
-=======
 import { CommessaListComponent } from './features/commessa/commessa-list/commessa-list.component';
 import { CommessaShowComponent } from './features/commessa/commessa-show/commessa-show.component';
 import { CommessaInsertComponent } from './features/commessa/commessa-insert/commessa-insert.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -38,22 +32,16 @@ import { CommessaInsertComponent } from './features/commessa/commessa-insert/com
     AziendaShowComponent,
     AziendaInsertComponent,
     RisorsaListComponent,
-<<<<<<< Updated upstream
     AziendaInsertComponent,
     AziendaEditComponent,
-<<<<<<< Updated upstream
-    AziendaDeleteComponent
-=======
+    AziendaDeleteComponent,
     RisorsaShowComponent,
     RisorsaInsertComponent,
-    RisorsaEditComponent
->>>>>>> Stashed changes
-=======
+    RisorsaEditComponent,
     AziendaDeleteComponent,
     CommessaListComponent,
     CommessaShowComponent,
     CommessaInsertComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
